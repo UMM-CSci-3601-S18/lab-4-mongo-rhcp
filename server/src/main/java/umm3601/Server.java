348e04b625c59957e93c3b31c2709131529484e0
blake376 <blake376@morris.umn.edu>
2018-02-22 15:58:02 -0600

    Refactored TodoController/RequestHandler

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -6,6 +6,8 @@ import spark.Request;
 import spark.Response;
 import umm3601.user.UserController;
 import umm3601.user.UserRequestHandler;
+import umm3601.todo.TodoController;
+import umm3601.todo.TodoRequestHandler;
 
 import java.io.IOException;
 
@@ -15,15 +17,19 @@ import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Server {
     private static final String userDatabaseName = "dev";
+    private static final String todoDatabaseName = "dev";
     private static final int serverPort = 4567;
 
     public static void main(String[] args) throws IOException {
 
         MongoClient mongoClient = new MongoClient();
         MongoDatabase userDatabase = mongoClient.getDatabase(userDatabaseName);
+        MongoDatabase todoDatabase = mongoClient.getDatabase(todoDatabaseName);
 
         UserController userController = new UserController(userDatabase);
         UserRequestHandler userRequestHandler = new UserRequestHandler(userController);
+        TodoController todoController = new TodoController(todoDatabase);
+        TodoRequestHandler todoRequestHandler = new TodoRequestHandler(todoController);
 
         //Configure Spark
         port(serverPort);
@@ -66,6 +72,9 @@ public class Server {
         get("api/users", userRequestHandler::getUsers);
         get("api/users/:id", userRequestHandler::getUserJSON);
         post("api/users/new", userRequestHandler::addNewUser);
+        get("api/todos", todoRequestHandler::getTodos);
+        get("api/todos/:id", todoRequestHandler::getTodoJSON);
+        post("api/todos/new", todoRequestHandler::addNewTodo);
 
         // An example of throwing an unhandled exception so you can see how the
         // Java Spark debugger displays errors like this.
--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,4 +1,120 @@
 package umm3601.todo;
 
+import com.google.gson.Gson;
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
+
+/**
+ * Controller that manstatuss requests for info about todos.
+ */
 public class TodoController {
+
+    private final Gson gson;
+    private MongoDatabase database;
+    private final MongoCollection<Document> todoCollection;
+
+    /**
+     * Construct a controller for todos.
+     *
+     * @param database the database containing todo data
+     */
+    public TodoController(MongoDatabase database) {
+        gson = new Gson();
+        this.database = database;
+        todoCollection = database.getCollection("todos");
+    }
+
+    /**
+     * Helper method that gets a single todo specified by the `id`
+     * parameter in the request.
+     *
+     * @param id the Mongo ID of the desired todo
+     * @return the desired todo as a JSON object if the todo with that ID is found,
+     * and `null` if no todo with that ID is found
+     */
+    public String getTodo(String id) {
+        FindIterable<Document> jsonTodos
+            = todoCollection
+            .find(eq("_id", new ObjectId(id)));
+
+        Iterator<Document> iterator = jsonTodos.iterator();
+        if (iterator.hasNext()) {
+            Document todo = iterator.next();
+            return todo.toJson();
+        } else {
+            // We didn't find the desired todo
+            return null;
+        }
+    }
+
+
+    /** Helper method which iterates through the collection, receiving all
+     * documents if no query parameter is specified. If the status query parameter
+     * is specified, then the collection is filtered so only documents of that
+     * specified status are found.
+     *
+     * @param queryParams
+     * @return an array of Todos in a JSON formatted string
+     */
+    public String getTodos(Map<String, String[]> queryParams) {
+
+        Document filterDoc = new Document();
+
+       /* if (queryParams.containsKey("status")) {
+            boolean targetStatus = Integer.parseInt(queryParams.get("status")[0]);
+            filterDoc = filterDoc.append("status", targetStatus);
+        } */
+
+        if (queryParams.containsKey("owner")) {
+            String targetContent = (queryParams.get("owner")[0]);
+            Document contentRegQuery = new Document();
+            contentRegQuery.append("$regex", targetContent);
+            contentRegQuery.append("$options", "i");
+            filterDoc = filterDoc.append("owner", contentRegQuery);
+        }
+
+        //FindIterable comes from mongo, Document comes from Gson
+        FindIterable<Document> matchingTodos = todoCollection.find(filterDoc);
+
+        return JSON.serialize(matchingTodos);
+    }
+
+
+    /**
+     * Helper method which appends received todo information to the to-be added document
+     *
+     * @param category
+     * @param status
+     * @param owner
+     * @param body
+     * @return boolean after successfully or unsuccessfully adding a todo
+     */
+    public String addNewTodo(String category, boolean status, String owner, String body) {
+
+        Document newTodo = new Document();
+        newTodo.append("category", category);
+        newTodo.append("status", status);
+        newTodo.append("owner", owner);
+        newTodo.append("body", body);
+
+        try {
+            todoCollection.insertOne(newTodo);
+            ObjectId id = newTodo.getObjectId("_id");
+            System.err.println("Successfully added new todo [_id=" + id + ", category=" + category + ", status=" + status + " owner=" + owner + " body=" + body + ']');
+            // return JSON.serialize(newTodo);
+            return JSON.serialize(id);
+        } catch(MongoException me) {
+            me.printStackTrace();
+            return null;
+        }
+    }
 }
