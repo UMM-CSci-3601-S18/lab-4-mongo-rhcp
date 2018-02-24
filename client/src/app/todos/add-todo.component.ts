23673d512f436cb612c8cb602fdc18e559d4c015
blake376 <blake376@morris.umn.edu>
2018-02-23 19:37:53 -0600

    Changed to mimic updated "add user" file

--- client/src/app/todos/add-todo.component.ts
+++ client/src/app/todos/add-todo.component.ts
@@ -12,8 +12,18 @@ export class AddTodoComponent {
         public dialogRef: MatDialogRef<AddTodoComponent>,
         @Inject(MAT_DIALOG_DATA) public data: {todo: Todo}) {
     }
+    public todoStatus : boolean;
 
     onNoClick(): void {
         this.dialogRef.close();
+    }
+
+    StatusRefactor(inputStatus: boolean) {
+        if (inputStatus) {
+            return 'Complete';
+        }
+        else {
+            return 'Incomplete';
+        }
     }
 }
